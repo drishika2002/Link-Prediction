08220d2ea59fd6461b4288da94dff5ac491ce79e
Drishika Chauhan <drishika.dcs16@gmail.com>
2023-11-26 18:15:34 +0530

    initial graph code added

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from flask import Flask, render_template, request
+import networkx as nx
+import matplotlib.pyplot as plt
+import io
+import base64
 
 app = Flask(__name__)
-app.static_folder = 'static' 
+app.static_folder = 'static'
 
 @app.route('/')
 def index():
@@ -10,15 +14,37 @@ def index():
 @app.route('/display_graph', methods=['POST'])
 def display_graph():
     num_edges = int(request.form['num_edges'])
-    edges = []
+    source_nodes = []
+    destination_nodes = []
 
     for i in range(num_edges):
-        x = float(request.form[f'x{i}'])
-        y = float(request.form[f'y{i}'])
-        edges.append((x, y))
+        source_key = f'source{i}'
+        destination_key = f'destination{i}'
 
-    return render_template('display_graph.html', num_edges=num_edges, edges=edges)
+        if source_key in request.form and destination_key in request.form:
+            source = request.form[source_key]
+            destination = request.form[destination_key]
+            source_nodes.append(source)
+            destination_nodes.append(destination)
+
+    # Create directed graph using NetworkX
+    G = nx.DiGraph()
+    for source, destination in zip(source_nodes, destination_nodes):
+        G.add_edge(source, destination)
+
+    # Plot the directed graph using Matplotlib
+    pos = nx.spring_layout(G)
+    nx.draw(G, pos, with_labels=True, node_size=700, node_color='skyblue', font_size=8, font_color='black', arrowsize=10)
+    
+    # Save the plot to a BytesIO object
+    img_buf = io.BytesIO()
+    plt.savefig(img_buf, format='png')
+    img_buf.seek(0)
+    
+    # Encode the image to base64 for displaying in HTML
+    img_data = base64.b64encode(img_buf.read()).decode('utf-8')
+
+    return render_template('display_graph.html', graph_image=img_data)
 
 if __name__ == '__main__':
     app.run(debug=True)
-
